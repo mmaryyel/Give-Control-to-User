f7a25810907de377134ceae7ce21036e7d8997b3
Maryel Malpica DiegoPerez <mmaryyel@gmail.com>
2017-02-13 20:53:12 -0600

    Form in CSS than can be controlled by JS to modify the page

---
+++ style.js
@@ -0,0 +1,11 @@
+$(function(){
+  $("form").on("submit", function(event){
+  	event.preventDefault();
+		var selector = $("input[name=selector]").val();
+		var property = $("input[name=property]").val();
+		var value = $("input[name=value]").val();
+		$(selector).css(property, value);
+  });
+});
+
+
